6abfe18415d9e188e51ef19620ad6f4708a867d4
Metallifax <61431322+Metallifax@users.noreply.github.com>
2020-10-20 20:02:50 -0300

    first commit

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import Reader  from './CsvFileReader';
+import { MatchResult } from './MatchResult';
+
+// Parse football.csv to 2d array
+const reader = new Reader('football.csv');
+reader.read();
+
+// Instantiate variable for Man United win analysis
+let manUnitedWins = 0;
+
+// For loop to analyze the parsed file for wins
+for (let match of reader.data) {
+  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
+    manUnitedWins++;
+  } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
+    manUnitedWins++;
+  }
+}
+
+// Log the results
+console.log(`Man United won ${manUnitedWins} games`);
