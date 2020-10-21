d76fe6e22b5c05cf76923ca28923b90d1a3f9ea9
Metallifax <61431322+Metallifax@users.noreply.github.com>
2020-10-20 21:44:51 -0300

    finishing up the application

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,20 @@
 import { CsvFileReader }  from './CsvFileReader';
 import { MatchResult } from './MatchResult';
+import { MatchReader } from './MatchReader';
 
-// Parse football.csv to 2d array
-const reader = new CsvFileReader('football.csv');
-reader.read();
+// Create an object that satisifies the 'DataReader' interface
+const csvFileReader = new CsvFileReader('football.csv');
+
+// Create an instance of MatchReader and pass in something satisfying
+// the 'DataReader' interface
+const matchReader = new MatchReader(csvFileReader);
+matchReader.load();
 
 // Instantiate variable for Man United win analysis
 let manUnitedWins = 0;
 
 // For loop to analyze the parsed file for wins
-for (let match of reader.data) {
+for (let match of matchReader.matches) {
   if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
     manUnitedWins++;
   } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
